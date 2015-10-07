317f7c772401622b029765613ae95a8992c4efa7
WhaleChen <18621050278@163.com>
2015-10-07 09:25:45 +0800

    add 3 files.

---
+++ ex3.py
@@ -0,0 +1,15 @@
+print "I will now count my chickens:"
+print "Hens", 25 +30/6
+print "Now I will count the eggs:"
+print 3.0+2.0+1.0-5.0+4%2-1.0/4.0+6.0
+# I guest the answer is 6.75?
+print "Is it true that 3+2<5-7?"
+# yes
+print 3 +2 <5-7
+print "What is 3+2?", 3+2
+print "What is 5-7?", 5-7
+print "Oh, that's why it's False."
+print "how about some more."
+print "Is it greater?", 5>-2
+print "Is it greater or equal?", 5>=-2
+print "Is it less or equal?", 5<= -2
